b24a07e69d7fd9a396a0f71edf601b779de97516
Guanhua Ding <guanhua.ding@go1.com>
2023-06-08 18:58:21 +1000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd ~/portfolio
 git pull origin main
 npm install &&
