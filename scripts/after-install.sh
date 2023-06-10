741c775f9228d868179bf1a52bfa2efb980b4636
Guanhua Ding <guanhua.ding@go1.com>
2023-06-10 21:02:54 +1000

    add git pull

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,6 @@
- #!/bin/bash
-  cd ~/portfolio || exit
-  rm -rf node_modules/
-  rm -rf .next
-  yarn install
+#!/bin/bash
+cd ~/portfolio || exit
+git pull origin main
+rm -rf node_modules/
+rm -rf .next
+yarn install
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-  nvm install 18
-  npm install --global yarn
-  npm install pm2 -g
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 18
+npm install --global yarn
+npm install pm2 -g
 
-  ln -sf $(which npm) /usr/local/bin/npm
-  ln -sf $(which node) /usr/local/bin/node
-  ln -sf $(which yarn) /usr/local/bin/yarn
-  ln -sf $(which pm2) /usr/local/bin/pm2
+ln -sf $(which npm) /usr/local/bin/npm
+ln -sf $(which node) /usr/local/bin/node
+ln -sf $(which yarn) /usr/local/bin/yarn
+ln -sf $(which pm2) /usr/local/bin/pm2
