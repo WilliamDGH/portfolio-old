c3b4cb17cc71e149b658cafd76c4321e1250b4d2
Guanhua Ding <guanhua.ding@go1.com>
2023-06-12 23:27:28 +1000

    add log

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,9 @@
 cd /var/www/guanhua-ding-portfolio
 
 # Start your application with PM2
-yarn install &&
+#!/bin/bash
+echo "$(date) - Running install_dependencies.sh" >> /var/log/deploy.log
+yarn install >> /var/log/deploy.log 2>&1
+echo "$(date) - Finished running install_dependencies.sh" >> /var/log/deploy.log
+
 pm2 start yarn --name "guanhua-ding-portfolio" -- start
