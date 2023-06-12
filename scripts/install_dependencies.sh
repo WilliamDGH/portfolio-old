b44f310518d9798327f86a78afa43d9e985918a4
Guanhua Ding <guanhua.ding@go1.com>
2023-06-12 23:45:32 +1000

    remove verbose

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /var/www/guanhua-ding-portfolio
+yarn install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,10 +3,4 @@
 cd /var/www/guanhua-ding-portfolio
 
 # Start your application with PM2
-#!/bin/bash
-echo "$(date) - Running install_dependencies.sh" >> /var/log/deploy.log
-cd /var/www/guanhua-ding-portfolio
-yarn install --verbose >> /var/log/deploy.log
-echo "$(date) - Finished running install_dependencies.sh" >> /var/log/deploy.log
-
 pm2 start yarn --name "guanhua-ding-portfolio" -- start
