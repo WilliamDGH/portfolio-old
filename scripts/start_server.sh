4c08db5c717ce3937d96298e47ae5336507647d7
Guanhua Ding <guanhua.ding@go1.com>
2023-06-12 23:34:46 +1000

    add verbose

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,8 @@ cd /var/www/guanhua-ding-portfolio
 # Start your application with PM2
 #!/bin/bash
 echo "$(date) - Running install_dependencies.sh" >> /var/log/deploy.log
-yarn install >> /var/log/deploy.log 2>&1
+cd /var/www/guanhua-ding-portfolio
+yarn install --verbose >> /var/log/deploy.log
 echo "$(date) - Finished running install_dependencies.sh" >> /var/log/deploy.log
 
 pm2 start yarn --name "guanhua-ding-portfolio" -- start
